53675fdc02502a857f5c3c3c8e285dd1f9a33d4d
Anutosh <anutosh.chaudhuri@gmail.com>
2020-12-17 17:18:56 +0530

    Footer Added

---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LandingRoutingModule } from './landing-routing.module';
+import { LandingComponent } from './landing.component';
+import {FooterModule} from '../shared/components/footer/footer.module';
+
+
+@NgModule({
+  declarations: [LandingComponent],
+    imports: [
+        CommonModule,
+        LandingRoutingModule,
+        FooterModule
+    ]
+})
+export class LandingModule { }
